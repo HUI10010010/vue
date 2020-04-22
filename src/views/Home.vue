04e5f7d9c61b02e420c8c48b0b47b51ffe8f0e3e
MingHui.Yang <2470400830@qq.com>
2020-04-22 16:55:51 +0800

    MING

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,7 @@
 
           <van-tab v-for="index in categories.length" :title="categories[index-1].name" class="tab">
 
-            <van-card v-for="(item,index) in phones"
-                      :price="item.price"
-                      :desc="item.desc"
-                      :title="item.title"
-                      :thumb="item.thumb"
-            >
+            <van-card v-for="(item,index) in phones" :price="item.price" :desc="item.desc" :title="item.title" :thumb="item.thumb">
               <template #tags>
                 <van-tag v-for="tag in item.tag" color="#f2826a" style="margin-left: 5px;">{{tag.name}}</van-tag>
               </template>
@@ -26,23 +21,12 @@
       </van-col>
     </van-row>
 
-    <van-sku
-            v-model="show"
-            :sku="sku"
-            :goods="goods"
-            :hide-stock="sku.hide_stock"
-            @buy-clicked="onBuyClicked"
-    >
+    <van-sku v-model="show" :sku="sku" :goods="goods" :hide-stock="sku.hide_stock" @buy-clicked="onBuyClicked">
       <template #sku-actions="props">
         <div class="van-sku-actions">
 
           <!-- 直接触发 sku 内部事件，通过内部事件执行 onBuyClicked 回调 -->
-          <van-button
-                  square
-                  size="large"
-                  type="danger"
-                  @click="props.skuEventBus.$emit('sku:buy')"
-          >
+          <van-button square size="large" type="danger" @click="props.skuEventBus.$emit('sku:buy')">
             买买买
           </van-button>
         </div>
@@ -51,58 +35,57 @@
 
   </div>
 
-
 </template>
 
 <script>
-  import {
-    Toast,
-    PullRefresh,
-    Swipe,
-    SwipeItem
-  } from 'vant';
-  export default {
-    comments:{
-      [PullRefresh.name]: PullRefresh,
-      [Swipe.name]: Swipe,
-      [SwipeItem.name]: SwipeItem
-    },
-    data() {
-      return {
-        categories: '',
-        phones: '',
-        show: true,
-        sku: '',
-        goods: ''
-      }
-    },
-    created(){
+import {
+  Toast,
+  PullRefresh,
+  Swipe,
+  SwipeItem
+} from 'vant';
+export default {
+  comments: {
+    [PullRefresh.name]: PullRefresh,
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem
+  },
+  data () {
+    return {
+      categories: '',
+      phones: '',
+      show: true,
+      sku: '',
+      goods: ''
+    }
+  },
+  created () {
+    const _this = this
+    axios.get('http://localhost:8181/phone/index').then(function (resp) {
+      _this.phones = resp.data.data.phones
+      _this.categories = resp.data.data.categories
+    })
+  },
+  methods: {
+    onClick (index) {
       const _this = this
-      axios.get('http://localhost:8181/phone/index').then(function (resp) {
-        _this.phones = resp.data.data.phones
-        _this.categories = resp.data.data.categories
+      axios.get('http://localhost:8181/phone/findByCategoryType/' + this.categories[index].type).then(function (resp) {
+        _this.phones = resp.data.data
       })
     },
-    methods: {
-      onClick(index) {
-        const _this = this
-        axios.get('http://localhost:8181/phone/findByCategoryType/'+this.categories[index].type).then(function (resp) {
-          _this.phones = resp.data.data
-        })
-      },
-      buy(index){
-        this.show = true
-        const _this = this
-        axios.get('http://localhost:8181/phone/findSpecsByPhoneId/'+this.phones[index].id).then(function (resp) {
-          _this.goods = resp.data.data.goods
-          _this.sku = resp.data.data.sku
-        })
-      },
-      onBuyClicked(item){
-        this.$store.state.specsId = item.selectedSkuComb.s1
-        this.$store.state.quantity = item.selectedNum
-        this.$router.push('/addressList')
-      }
+    buy (index) {
+      this.show = true
+      const _this = this
+      axios.get('http://localhost:8181/phone/findSpecsByPhoneId/' + this.phones[index].id).then(function (resp) {
+        _this.goods = resp.data.data.goods
+        _this.sku = resp.data.data.sku
+      })
+    },
+    onBuyClicked (item) {
+      this.$store.state.specsId = item.selectedSkuComb.s1
+      this.$store.state.quantity = item.selectedNum
+      this.$router.push('/addressList')
     }
   }
+}
 </script>
